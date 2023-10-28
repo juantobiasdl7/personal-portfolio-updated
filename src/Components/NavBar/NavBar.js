414044addc50973959816adaf689bae681c9ecdc
Tobias <juantobiasdl7@gmail.com>
2023-10-28 15:48:09 -0600

    I fixed a responsive error of the nav bar.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -9,6 +9,7 @@ function NavigationBar() {
     // const [activeLink, setActiveLink] = useState('home');
     const [isScrolled, setIsScrolled] = useState(false);
     const [isExpanded, setIsExpanded] = useState(false);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
     useEffect(() => {
     const handleScroll = () => {
@@ -25,21 +26,49 @@ function NavigationBar() {
     // Attach the event listener
     window.addEventListener('scroll', handleScroll);
 
+    // Handler to call on window resize
+    function handleResize() {
+
+        const toggleButton = document.getElementById('toggle-button');
+        const expandButton = document.getElementById('basic-navbar-nav');
+        // Set state based on the viewport width
+        
+
+        if (window.innerWidth > 768) {
+            toggleButton.classList.add('collapsed');
+            expandButton.classList.remove('show');
+            setIsMobile(false);
+        } else {
+            setIsMobile(true);
+        }
+    }
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Call handler right away so state gets updated with initial viewport width
+    handleResize();
+
+
+
+
     // Cleanup: remove the event listener when the component is unmounted
     return () => {
         window.removeEventListener('scroll', handleScroll);
+        window.removeEventListener('resize', handleResize);
     };
     }, []);  // Empty dependency array means this useEffect will run once after component mounts
 
 
+
     return (
-        <Navbar expand="md" fixed="top" className= {` ${isScrolled ? "bg-color" : isExpanded ? "bg-color" : "bg-transparent"}`} >
+        <Navbar expand="md" fixed="top" className= {` ${isScrolled ? "bg-color" : isExpanded && isMobile ? "bg-color" : "bg-transparent"}`} >
             <Container>
             <Navbar.Brand href="#home" className="text-light" style={{fontSize:"22px"}}>
                 <Braces />
                 {" "}Tobias.dev
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" className="text-light" onClick={() => setIsExpanded(!isExpanded)}>
+            <Navbar.Toggle id="toggle-button" aria-controls="basic-navbar-nav" className="text-light" onClick={() => setIsExpanded(!isExpanded)}>
                 <MenuButtonWide />
             </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
